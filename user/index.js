628c06eb20ceef458557c599ec3a2bc293efe112
Komarov Alexander <a.kom90@mail.ru>
2017-12-13 13:28:36 +0300

    methods of working with modules

--- user/index.js
+++ user/index.js
@@ -1,15 +1,18 @@
-var phrases = require('./ru.json');
+var db = require('db');
+var log = require('logger')(module);
 
 function User(name) {
 	this.name = name;
 }
 
 User.prototype.hello = function (who) {
-	console.log(phrases.Hello + ', ' + who.name );
+	log(db.getPhrase('Hello') + ', ' + who.name );
 };
 
-console.log('user.js is required');
 //exports
-exports.User = User;
+// exports.User = User;
+module.exports = User;
 //global
 // global.User = User;
+
+// console.log(module);
