061a6edc7d9e9bc825d2fccd37204271882e45be
Komarov Alexander <a.kom90@mail.ru>
2017-12-13 17:44:24 +0300

    echo-server on node js

--- server.js
+++ server.js
@@ -1,18 +1,37 @@
-var http = require('http');
+//http://127.0.0.1/echo?message=Hello -> Hello
 
-var server = new http.Server();
-//http.Server -> net.Server -> EventEmitter
+var http = require('http');
+var url = require('url');
+
+var server = new http.Server(function (req, res) {
+
+	console.log( req.headers );
+
+	var urlParsed = url.parse(req.url, true);
+	console.log(urlParsed);
+
+	if (urlParsed.pathname == '/echo' && urlParsed.query.message){
+		// res.writeHead(200, 'OK', {'Cache-control': 'no-cache'});
+		res.setHeader('Cache-control', 'no-cache');
+		res.end( urlParsed.query.message );
+	} else {
+		res.statusCode = 404;
+		res.end("Page not found");
+	}
+
+	/*
+	console.log(req.method, req.url);
+
+	var urlParsed = url.parse(req.url, true);
+	console.log(urlParsed);
+
+	if (urlParsed.pathname == '/echo' && urlParsed.query.message){
+		res.end( urlParsed.query.message );
+	} else {
+		res.statusCode = 404;
+		res.end("Page not found");
+	}
+	*/
+});
 
 server.listen(1337, '127.0.0.1');
-
-var counter = 0;
-
-var emit = server.emit;
-server.emit = function (event) {
-	console.log(event);
-	emit.apply(server, arguments);
-};
-
-server.on('request', function (req, res) {
-	res.end('Привет мир! ' + ++counter);
-});
