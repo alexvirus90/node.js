13f521a94f9f739ec99e432788f6b1775fa3b828
Komarov Alexander <a.kom90@mail.ru>
2017-12-14 11:27:37 +0300

    module logger is winston

--- server.js
+++ server.js
@@ -1,39 +1,9 @@
-//http://127.0.0.1/echo?message=Hello -> Hello
-
 var http = require('http');
-var url = require('url');
+var log = require('./log')(module);
 
-/*
-var server = new http.Server(function (req, res) {
+var server = http.createServer();
 
-	console.log( req.headers );
+server.on('request', require('./request'));
+server.listen(1337);
 
-	var urlParsed = url.parse(req.url, true);
-	console.log(urlParsed);
-
-	if (urlParsed.pathname == '/echo' && urlParsed.query.message){
-		// res.writeHead(200, 'OK', {'Cache-control': 'no-cache'});
-		res.setHeader('Cache-control', 'no-cache');
-		res.end( urlParsed.query.message );
-	} else {
-		res.statusCode = 404;
-		res.end("Page not found");
-	}
-	*/
-
-	/*
-	console.log(req.method, req.url);
-
-	var urlParsed = url.parse(req.url, true);
-	console.log(urlParsed);
-
-	if (urlParsed.pathname == '/echo' && urlParsed.query.message){
-		res.end( urlParsed.query.message );
-	} else {
-		res.statusCode = 404;
-		res.end("Page not found");
-	}
-	*/
-// });
-
-server.listen(1337, '127.0.0.1');
+log.debug("Server is running");
