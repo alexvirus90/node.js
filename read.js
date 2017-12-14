2d6e62c24f5e7efb749fa499d11f38eaff0d279f
Komarov Alexander <a.kom90@mail.ru>
2017-12-14 19:18:10 +0300

    method pipe

--- read.js
+++ read.js
@@ -20,21 +20,22 @@ new http.Server(function (req, res) {
 }).listen(3000);
 
 function sendFile(file, res) {
-	file.on('readable', write);
+	file.pipe(res);
+	
+	file.on('error', function (err) {
+		res.statusCode = 500;
+		res.end('Server Error');
+		console.error(err);
+	});
 
-	function write() {
-		var fileContent = file.read();
-
-		if (fileContent && !res.write(fileContent)) {
-			file.removeListener('readable', write);
-
-			res.once('drain', function () {
-				file.on('readable', write);
-				write();
-			});
-		}
-	}
-	file.on('end', function () {
-		res.end();
-	})
+	file
+		.on('open', function () {
+			console.log('open');
+		})
+		.on('close', function () {
+			console.log('close');
+		});
+	res.on('close', function () {
+		file.destroy();
+	});
 }
